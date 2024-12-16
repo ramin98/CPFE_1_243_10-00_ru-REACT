f8846134694d258bd9553f7008d4f56681767843
ramin98 <59912261+ramin98@users.noreply.github.com>
2024-12-16 10:40:50 +0400

    Add files via upload

--- react-shop/src/reducers/reducerStates/ordersReducer.js
+++ react-shop/src/reducers/reducerStates/ordersReducer.js
@@ -3,16 +3,8 @@ export let initialObjectOrders = {
 }
 
 export function ordersReducer(state, action) {
-    // if(action.type === 'ORDER FORM'){
-    //     let newArr = [state.orders]
-    //     let bag = JSON.parse(localStorage.getItem('bag'))
-    //     let data = {...action.payload, bag}
-    //     newArr.push(data)
-    //     addToOrders(data)
-    //     return {...state, orders: newArr}
-    // }
 
-    if(action.type === 'GET ORDERS'){
+ if(action.type === 'GET ORDERS'){
         console.log(action.payload)
         return {...state, orders: action.payload}
     }
